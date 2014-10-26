7d5a8fff6978dd153fc99eb42620c726f1ac21dc
shadelro <rbshadel@gmail.com>
2014-10-26 00:37:27 -0500

    Revert "let's send some emails"
    This reverts commit 39a38698d16f01004c71724201399bb354e6ef3b.

--- app/mailers/user_mailer.rb
+++
@@ -1,9 +0,0 @@
-class UserMailer < ActionMailer::Base
-  default from: 'bandaider@bandaider.tk'
-
-  def band_invitation_email(invitation)
-    @invitation = invitation
-
-    mail to: @invitation.user.email, subject: "You've been invited to join #{@invitation.band.name}!"
-  end
-end
